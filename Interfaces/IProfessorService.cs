780b107ec3282cde2629a0b9aff4bd3cf53bffc9
VoronkoIllia <ilyhavoronko@gmail.com>
2025-04-29 12:53:56 +0300

    Реалізовано функції AddNewProfessor та UpdateProfessor

--- Interfaces/IProfessorService.cs
+++ Interfaces/IProfessorService.cs
@@ -11,6 +11,9 @@ namespace ProfessorsSSU.Interfaces
     public interface IProfessorService
     {
         List<Professor> SelectProfessors(bool? hasAcademicRank = null, bool onlyPensioners = false);
+        
+        void AddNewProfessor(Professor professor);
+        bool UpdateProfessor(int professorId, Professor newProfessorData);
         bool DeleteProfessor(int professorId);
     }
 }
--- Services/ProfessorService.cs
+++ Services/ProfessorService.cs
@@ -22,14 +22,25 @@ namespace ProfessorsSSU.Services
             return _appDbContext.Professors.ToList();
         }
 
-        /// <summary>
-        /// видаляє за ідентифікатором викладача з БД  
-        /// </summary>
-        /// <param name="professorId">Ідентифікатор викладача, якого потрібно видалити</param>
-        /// <returns>
-        ///     true, якщо видалення пройшло успішно <br/>
-        ///     false, якщо викладача з таким ідентифікатором немає в БД
-        /// </returns>
+        public void AddNewProfessor(Professor professor) 
+        {
+            _appDbContext.Professors.Add(professor);
+            _appDbContext.SaveChanges();
+        }
+
+        public bool UpdateProfessor(int professorId, Professor newProfessorData)
+        {
+            Professor? professor = _appDbContext.Professors.FirstOrDefault((professor) => professor.Id == professorId);
+            if (professor == null)
+            {
+                return false;
+            }
+
+            _appDbContext.Entry(professor).CurrentValues.SetValues(newProfessorData);
+            _appDbContext.SaveChanges();
+            return true;
+        }
+
         public bool DeleteProfessor(int professorId)
         {
             Professor? professor = _appDbContext.Professors.FirstOrDefault((professor) => professor.Id == professorId);
@@ -37,12 +48,11 @@ namespace ProfessorsSSU.Services
             {
                 return false;
             }
-            else 
-            {
-                _appDbContext.Professors.Remove(professor);
-                _appDbContext.SaveChanges();
-                return true;
-            }
+            
+            _appDbContext.Professors.Remove(professor);
+            _appDbContext.SaveChanges();
+            return true;
+            
         }
     }
 }
